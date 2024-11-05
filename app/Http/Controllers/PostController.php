b09c28b99fbc5c892b1a74720d26b22703e0782a
Bysantosj17 <uriel_barbosasan@hotmail.com>
2024-11-05 17:42:27 -0600

    05/11/2024

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -76,7 +76,7 @@ class PostController extends Controller
     public function update(Request $request, Post $post)
     {
 
-        // $validated = $request->validate([
+        // $validated = $request->validatehola([
         //     // 'title' => ['required', 'min:4'],
         //     // 'body' => ['required']
         // ]/*,[
